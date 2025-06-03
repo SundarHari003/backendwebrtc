81c7276010a9c17a237b945d0f21e1c9299e9e14
SundarHari003 <sundarhari014@gmail.com>
2025-06-03 13:54:41 +0530

    new changes

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
-const https = require('https');
+// const https = require('https');
+const http = require('http');
 const { Server } = require('socket.io');
 const mediasoup = require('mediasoup');
 const cors = require('cors');
 const Room = require('./Room');
 const os = require('os');
 const ifaces = os.networkInterfaces()
-const fs = require('fs');
+// const fs = require('fs');
 
-const sslOptions = {
-  key: fs.readFileSync('./ssl/key.pem'),
-  cert: fs.readFileSync('./ssl/cert.pem'),
-};
+// const sslOptions = {
+//   key: fs.readFileSync('./ssl/key.pem'),
+//   cert: fs.readFileSync('./ssl/cert.pem'),
+// };
 
 const getLocalIp = () => {
   let localIp = '127.0.0.1'
@@ -38,7 +39,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const server = https.createServer(sslOptions,app);
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: '*',
